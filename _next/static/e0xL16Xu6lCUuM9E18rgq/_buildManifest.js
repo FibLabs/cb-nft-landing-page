1a07b3a2f466e15caeae1ad0816bf979bada8746
fibolabs <97525880+fibolabs@users.noreply.github.com>
2022-01-12 08:37:28 +0000

    Deploying to gh-pages from @ FibLabs/cb-nft-landing-page@c78c3ce93913b3ecf5df1b7797bbbeb3b01183c2 🚀

---
+++ _next/static/e0xL16Xu6lCUuM9E18rgq/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/111-5bda65de6850ec3a.js","static/chunks/pages/index-9e3c817df8aeb05d.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
